e6b531572353600781ceb21309315d999376e55f
haris-hadad <haris.hadad@laplateforme.io>
2026-04-30 14:12:39 +0200

    creation model type et ajout methode create pour ajouté un type

---
+++ backend/src/models/Types.js
@@ -0,0 +1,60 @@
+const BaseModel = require('./BaseModel');
+const db = require('../config/db');
+
+class Types extends BaseModel {
+   
+
+    constructor(data) {
+        super('types'); // On dit à la classe mère qu'on gère la table 'types'
+        Object.assign(this, data); // Astuce pour assigner tous les champs d'un coup
+    } 
+
+    // Création d'un types dans la table `typess`
+    static async create(data) {
+        const connection = await super.getConnection();
+
+        try {
+            const sql = `
+                INSERT INTO types (nom)
+                VALUES (?)
+            `;          
+
+            const [result] = await connection.execute(sql, [data.nom]);
+            return result.insertId;
+        } finally {
+            connection.release();
+        }
+    }
+
+    //modification d'un types
+    static async update(id, data) {
+        const connection = await super.getConnection();
+        try {
+            const sql = `
+                UPDATE typess 
+                SET nom = ?, email = ?, password = ?, avatar_url = ?, role = ?
+                WHERE id = ?
+            `;
+            const params = [
+                data.nom,
+                data.email,
+                data.password,
+                data.avatar_url,
+                data.role,
+                id
+            ];
+
+            const [result] = await connection.execute(sql, params);
+            return result.affectedRows > 0;
+        } finally {
+            connection.release();
+        }
+    }
+
+
+   
+
+    
+}
+
+module.exports = Types;
