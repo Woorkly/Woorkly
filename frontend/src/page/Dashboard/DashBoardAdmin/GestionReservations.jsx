750bfce7c912e1c98032d8dc2f444688dfd17dfd
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2026-05-25 16:01:45 +0200

    fix: casse des fichiers pour vercel

--- frontend/src/page/Dashboard/DashBoardAdmin/GestionReservations.jsx
+++ frontend/src/page/Dashboard/DashBoardAdmin/GestionReservations.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import "./adminStyle.css";
+import "./AdminStyle.css";
 
 const DAYS = [
   "Oct 15",
--- frontend/src/page/SalleDetail/SalleDetail.jsx
+++ frontend/src/page/SalleDetail/SalleDetail.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import "./salleDetail.css";
+import "./SalleDetail.css";
 
 const salles = [
   {
